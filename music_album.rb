965f9e8caed2bf8699325820a55d36dcaff2fc57
moise10r <moiserushanika2006@gmail.com>
2021-10-18 22:55:16 +0200

    Add the constructor method of musicAlbum class.

--- music_album.rb
+++ music_album.rb
@@ -1,2 +1,12 @@
 require_relative 'item'
 
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify, source, label, publish_date)
+    super(source, label, publish_date)
+    @on_spotify = on_spotify
+    can_be_archived
+  end
+
+end
