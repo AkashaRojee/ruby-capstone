d0fcba78c6c5b685b8d6dc2e2679213456a739d2
Akasha <Akasha.Rojee@gmail.com>
2021-10-18 16:34:40 +0400

    Add properties for 1-many relationships in Item

--- item.rb
+++ item.rb
@@ -1,11 +1,23 @@
 class Item
 
+  attr_accessor :genre, :author
+
   def initialize(source, label, publish_date)
     @id = Random.rand(0..10_000)
     @source = source
     @label = label
     @publish_date = publish_date
     @archived = false
+  end
+
+  def genre=(genre)
+    @genre = genre
+    # AKASHA: after creating Genre class - genre.items.push(self) 
+  end
+
+  def author=(author)
+    @author = author
+    # AKASHA: after creating Author class - author.items.push(self) 
   end
 
 end
