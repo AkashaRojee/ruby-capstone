a8e72c95eb17360b2f88476a7cdb5a85a1717fc8
moise10r <moiserushanika2006@gmail.com>
2021-10-19 20:36:41 +0200

    Remove source label and author method.

--- item.rb
+++ item.rb
@@ -22,33 +22,6 @@ class Item
     author.items.push(self) unless author.items.include?(self)
   end
 
-  def add_source(source)
-    @source = source
-    source.items.push(self) unless source.items.include?(self)
-  end
-
-  def add_label(label)
-    @label = label
-    label.items.push(self) unless label.items.include?(self)
-  end
-
-  def to_s
-    "ID: #{@id}, Author: #{@author}, Genre: #{@genre},  \
-    Source: #{@source}, Label: #{@label}, Publish Date: #{@publish_date}"
-  end
-
-
-  def to_json(_args)
-    {
-      'id' => @id,
-      'publish_date' => @publish_date,
-      'author_id' => @author.id,
-      'genre_id' => @genre.id,
-      'source_id' => @source.id,
-      'label_id' => @label.id
-    }
-  end
-
   def move_to_archive
     @archived = can_be_archived?
   end
