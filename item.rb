0bde100cf058af51a66e9a4a13bb1c0d490c35c7
Tolibjon <tolibjohn@gmail.com>
2021-10-19 08:43:52 +0500

    Create Game class and methods

--- item.rb
+++ item.rb
@@ -2,12 +2,12 @@ require 'date'
 
 class Item
 
-  attr_accessor :genre, :author
+  attr_accessor :genre, :author, :source
 
-  def initialize(source, label, publish_date)
+  def initialize( publish_date)
     @id = Random.rand(0..10_000)
-    @source = source
-    @label = label
+    # @source = source
+    # @label = label
     @publish_date = publish_date
     @archived = false
   end
@@ -22,13 +22,19 @@ class Item
     # AKASHA: after creating Author class - author.items.push(self) 
   end
 
+  def source=(source)
+    @source = source
+    source.items.push(self) 
+  end
+
   def move_to_archive
     @archived = can_be_archived?
   end
 
   private
   def can_be_archived?
-    (Date.today() - Date.parse(@publish_date)).to_i/365 > 10
+    Date.today.year - Date.parse(@publish_date).year > 10
+    # (Date.today() - Date.parse(@publish_date)).to_i/365 > 10
   end
 end
 
