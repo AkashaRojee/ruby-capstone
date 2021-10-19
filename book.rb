73de1c7ad8034c34f3ea8269ab3a0e3e605d30c3
Akasha <Akasha.Rojee@gmail.com>
2021-10-19 14:20:32 +0400

    Add Book constructor

---
+++ book.rb
@@ -0,0 +1,11 @@
+require_relative 'item'
+
+class Book < Item
+
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+end
