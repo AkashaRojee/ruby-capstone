f227411bdac9bab5a31c79c251b0a94745c2da1c
moise10r <moiserushanika2006@gmail.com>
2021-10-19 17:21:52 +0200

    Add author class with its constructor.

--- music_album.rb
+++ music_album.rb
@@ -11,4 +11,16 @@ class MusicAlbum < Item
   def can_be_archived?
     super && @on_spotify
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => id,
+      'on_spotify' => @on_spotify,
+      'source' => @source,
+      'label' => @label,
+      'archived' => @archived 
+      'publish_date' => @publish_date,
+    }.to_json(*args)
+  end
 end
