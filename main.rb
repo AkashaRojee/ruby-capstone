b9efb9c1c95e836261820b7ee49749155ef7ee85
Akasha <Akasha.Rojee@gmail.com>
2021-10-19 14:19:28 +0400

    Refactor main

--- main.rb
+++ main.rb
@@ -1,12 +1,51 @@
+require_relative 'catalog'
+
 class Main
+
+  def initialize
+    @catalog = Catalog.new
+  end
   
   def start_app
-    choice = show_menu
-    choice = show_menu while choice < 1 || choice > 10
+    @catalog.save_files
+    choice = 0;
 
+    until choice == 10
+      show_menu
+
+      choice = get_choice
+      choice = get_choice while choice < 1 || choice > 10
+
+      handle_choice(choice)
+    end
+
+  end
+
+  def show_menu
+    options = [
+      '1 - List all books',
+      '2 - List all music albums',
+      '3 - List all games',
+      '4 - List all labels',
+      '5 - List all genres',
+      '6 - List all authors',
+      '7 - Add a book',
+      '8 - Add a music album',
+      '9 - Add a game',
+      '10 - Exit'
+    ]
+    puts options
+  end
+
+  def get_choice
+    print 'Please choose an option by entering a number from 1 to 10: '
+    gets.chomp.to_i
+  end
+  
+  def handle_choice(choice)
     case choice
     when 1 
-      # method_1 call
+      @catalog.list_books
     when 2 
       # method_2 call
     when 3 
@@ -18,30 +57,14 @@ class Main
     when 6 
       # method_6 call
     when 7 
-      # method_7 call
+      @catalog.add_book
     when 8 
       # method_8 call
     when 9 
       # method_9 call
+    when 10
+      @catalog.save_files
     end
   end
-
-  def show_menu
-    options = ['1 - List all books',
-               '2 - List all music albums',
-               '3 - List all games',
-               '4 - List all labels',
-               '5 - List all genres',
-               '6 - List all authors',
-               '7 - Add a book',
-               '8 - Add a music album',
-               '9 - Add a game',
-               '10 - Exit'
-              ]
-
-    puts 'Please choose an option by entering a number: ', options
-    gets.chomp.to_i
-  end
-  
 end
 Main.new.start_app
