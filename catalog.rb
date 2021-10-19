5228b767060222b65b980a7c09d854e04c3dd33a
Akasha <Akasha.Rojee@gmail.com>
2021-10-19 14:26:09 +0400

    Add Book to catalog

---
+++ catalog.rb
@@ -0,0 +1,27 @@
+require_relative 'book'
+
+class Catalog
+
+  def initialize
+    @books = []
+  end
+
+  def add_book
+    puts 'ADD BOOK'
+
+    source = prompt('Source')
+    label = prompt('Label')
+    publish_date = prompt('Publish date')
+    publisher = prompt('Publisher')
+    cover_state = prompt('Cover state')
+
+    @books.push(Book.new(source, label, publish_date, publisher, cover_state))
+
+    puts 'Book added successfully'
+  end
+
+  def prompt(data_item)
+    print "#{data_item}: "
+    gets.chomp
+  end
+end
