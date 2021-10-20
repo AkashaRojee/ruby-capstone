24eded77835b067902d7c268dfdff7e95e80acf4
moise10r <moiserushanika2006@gmail.com>
2021-10-20 15:36:58 +0200

    Fix failed tests

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,14 +3,14 @@ require_relative '../music_album'
 describe MusicAlbum do
   describe '#can_be_archived' do
     context 'The interval between publish_date up to now  is over 10 years and on spotify' do
-      item = MusicAlbum.new(true, 'Youtube', 'Invisible', '1929-02-20')
+      item = MusicAlbum.new(true,'1929-02-20')
       it 'should return true' do
         expect(item.can_be_archived?).to be(true)
       end
     end
 
     context 'The interval between publish_date up to now  is over 10 years and not on spotify' do
-      item = MusicAlbum.new(false, 'Youtube', 'Invisible', '1929-02-20')
+      item = MusicAlbum.new(false, '1929-02-20')
       it 'should return false' do
         expect(item.can_be_archived?).to be(false)
       end
@@ -18,14 +18,14 @@ describe MusicAlbum do
 
     context 'The interval between publish_date up to now  is less that 10 years and on spotify' do
       it 'should return false' do
-        item = MusicAlbum.new(true, 'Youtube', 'Invisible', '2020-02-20')
+        item = MusicAlbum.new(true, '2020-02-20')
         expect(item.can_be_archived?).to be(false)
       end
     end
 
     context 'The interval between publish_date up to now  is less than 10 years and not on spotify' do
       it 'should return false' do
-        item = MusicAlbum.new(false, 'Youtube', 'Invisible', '2020-02-20')
+        item = MusicAlbum.new(false, '2020-02-20')
         expect(item.can_be_archived?).to be(false)
       end
     end
