50c3abb1661dae435679b6cad8290211b6476697
moise10r <moiserushanika2006@gmail.com>
2021-10-20 02:53:56 +0200

    Add the genres table

--- schema.sql
+++ schema.sql
@@ -8,8 +8,14 @@ CREATE TABLE music_albums (
     source VARCHAR(100),
     label VARCHAR(100),
     archived BOOLEAN,
-    on_spotify: BOOLEAN
     publish_date DATE,
     PRIMARY KEY(id),
     FOREIGN KEY (genre_id) REFERENCES genres (id),
+)
+
+CREATE TABLE genres (
+    id  INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    on_spotify: BOOLEAN,
+    PRIMARY KEY(id)
 )
