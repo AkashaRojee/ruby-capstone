12072ba6f6e6ad95699ef5a4b7993cb0a810d26a
Tolibjon <tolibjohn@gmail.com>
2021-10-20 15:50:57 +0500

    Add tests for Book and Label classes

---
+++ spec/book_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../book'
+
+describe Book do
+  before :each do
+    @book = Book.new '2012/02/02', 'ProfPress', 'bad'
+  end
+
+  describe '#new' do
+    it 'takes three parameters and returns a Book object' do
+      expect(@book).to be_an_instance_of(Book)
+    end
+  end
+
+  describe '#archived' do
+    it 'returns false (by default set as false)' do
+      expect(@book.archived).to eql false
+    end
+  end
+
+  describe '#can_be_archived?' do
+    it 'returns true if parent\'s method returns true OR if cover_state equals to "bad"' do
+      @book.move_to_archive
+      expect(@book.archived).to eql true
+    end
+  end
+end
