1609b0e80315acb51c5b050525960fb3e95c8a5a
Akasha <Akasha.Rojee@gmail.com>
2021-10-20 20:46:40 +0400

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,50 +1,43 @@
-CREATE DATABASE catalog
+CREATE DATABASE catalog;
+
+CREATE TABLE genre (
+    id    INT GENERATED ALWAYS AS IDENTITY,
+    name  VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE author (
+    id          INT GENERATED ALWAYS AS IDENTITY,
+    first_name  VARCHAR(50)
+    last_name   VARCHAR(50)
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE games (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    publish_date DATE
-    genre_id INT
-    author_id INT
-    source_id INT
-    label_id INT
-    archived BOOLEAN
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    multiplayer   BOOLEAN,
+    last_played_at DATE,
+    publish_date  DATE
+    genre_id      INT
+    author_id     INT
+    label_id      INT
+    archived      BOOLEAN
     PRIMARY KEY(id)
     FOREIGN KEY(genre_id) REFERENCES genre(id)
     FOREIGN KEY(author_id) REFERENCES author(id)
-    FOREIGN KEY(source_id) REFERENCES source(id)
     FOREIGN KEY(label_id) REFERENCES label(id)
-)
-
-CREATE TABLE author (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    first_name VARCHAR(50)
-    last_name VARCHAR(50)
-    PRIMARY KEY(id)
-)
-
-CREATE TABLE source (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(50)
-    PRIMARY KEY(id)
-)
-CREATE DATABASE 'catalog';
-
-
-CREATE TABLE music_albums (
-    id  INT GENERATED ALWAYS AS IDENTITY,
-    on_spotify BOOLEAN,
-    genre VARCHAR(100),
-    source VARCHAR(100),
-    label VARCHAR(100),
-    archived BOOLEAN,
-    publish_date DATE,
-    PRIMARY KEY(id),
-    FOREIGN KEY (genre_id) REFERENCES genres (id),
 );
 
-CREATE TABLE genres (
-    id  INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    on_spotify: BOOLEAN,
-    PRIMARY KEY(id)
+CREATE TABLE music_albums (
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    on_spotify    BOOLEAN,
+    publish_date  DATE,
+    genre_id      INT,
+    author_id     INT,
+    label_id      INT,
+    archived      BOOLEAN,
+    PRIMARY KEY(id),
+    FOREIGN KEY (genre_id) REFERENCES genres (id),
+    FOREIGN KEY (author_id) REFERENCES authors (id),
+    FOREIGN KEY (label_id) REFERENCES labels (id)
 );
