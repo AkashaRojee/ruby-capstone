859465c8c61b71f793e520bbb925f268f0371954
moise10r <moiserushanika2006@gmail.com>
2021-10-20 11:46:52 +0200

    Add an id on music album.

--- music_album.rb
+++ music_album.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(on_spotify, _source, _label, publish_date)
-    super(_source, _label, publish_date)
+  def initialize(on_spotify, source, label, publish_date)
+    super(source, label, publish_date)
     @on_spotify = on_spotify
   end
 
@@ -13,23 +13,24 @@ class MusicAlbum < Item
   end
 
   def to_s
-    "\nAlbum On Spotify: #{@on_spotify}, \n source: #{@_source} \n label: #{@_label} \n
+    "\nAlbum On Spotify: #{@on_spotify}, \n source: #{@source} \n label: #{@label} \n
      archived: #{@archived} \n publish_date: #{@publish_date} \n #{super}\n"
   end
 
   def to_json(*args)
                 {  JSON.create_id => self.class.name,
+                  'id' => @id,
                   'on_spotify' => @on_spotify,
-                  'source' => @_source,
-                  'label' => @_label,
+                  'source' => @source,
+                  'label' => @label,
                   'archived' => @archived,
                   'publish_date' => @publish_date
                 }.to_json(*args)
   end
 
   def self.json_create(object)
-    music_album = new(object['on_spotify'], object['source'], object['label'], object['archived'],
-                      Time.parse(object['publish_date']))
+    music_album = new(object['on_spotify'], object['source'], object['label'],
+                      Date.parse(object['publish_date']))
     music_album.id = object['id']
     music_album
   end
